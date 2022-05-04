8d0f914bda25732e2e56e398f6b7c92ab075efe9
python019 <tommyjumayev@gmail.com>
2022-05-04 12:19:11 +0500

    7-dars

--- js/main.js
+++ js/main.js
@@ -3,18 +3,10 @@
 let a = 18
 let b = 8
 
-console.log(a == b)
-console.log(a === b)
-console.log(a > b)
-console.log(a < b)
-console.log(a <= b)
-console.log(a >= b)
-console.log(a != b)
+console.log((a >= b) && (a > b))
+console.log((a >= b) || (a < b))
+console.log(!(a >= b))
 
-// == tenglik
-// === mutlaqo tenglik
-// > katta
-// < kichik
-// <= kichik yoki teng
-// >== katta yoki teng
-// != teng emas
+// && 
+// ||
+// !
