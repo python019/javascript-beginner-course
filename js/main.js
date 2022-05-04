a4d79502c209407deeca3b2ee40acb0c8ed6db51
python019 <tommyjumayev@gmail.com>
2022-05-04 11:53:29 +0500

    6-dars

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 // JavaScript Operators
 
 let a = 18
-let b = 5
+let b = 8
 
-console.log(a + b)
-console.log(a - b)
-console.log(a * b)
-console.log(a / b)
-console.log(++a)
-console.log(--a)
+console.log(a == b)
+console.log(a === b)
+console.log(a > b)
+console.log(a < b)
+console.log(a <= b)
+console.log(a >= b)
+console.log(a != b)
 
-// +
-// -
-// *
-// /
-// %
-// ++
-// --
+// == tenglik
+// === mutlaqo tenglik
+// > katta
+// < kichik
+// <= kichik yoki teng
+// >== katta yoki teng
+// != teng emas
