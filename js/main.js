2472f957063e803fb61120f7d07b2bd9d6682b6c
python019 <tommyjumayev@gmail.com>
2022-05-06 15:30:42 +0500

    10-dars

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
-// JavaScript Operators
-
-let a = 18
-let b = 18
-
-c = a == b ? "teng" : "teng emas"
-
-console.log(c)
-
-// ? 
-// :
+// JavaScript Shart Operatorlari
 
 
+a = 30
+b = 30
 
+if (a > b) {
+   console.log("a katta b dan");
+}
+else if (a == b) {
+    console.log("a va b teng");
+}
+else {
+   console.log("b katta a dan");
+}
 
+yil = 2007
+hozir = 2022
+yosh = hozir - yil
+
+if (yosh > 18) {
+   console.log("To'laqonli o'zbek fuqarosi");
+}
+else if (yosh > 16) {
+    console.log("Pasportga ega fuqaro");
+}
+else {
+   console.log("Hali kichkinamiz");
+}
