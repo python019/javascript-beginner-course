450a03672b25708cd119521cf548da6e7e673538
python019 <tommyjumayev@gmail.com>
2022-05-14 10:38:06 +0500

    18-dars

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
-// JavaScript Arrays
+// JavaScript Browser Object Model (BOM)
 
+console.log(window.innerWidth)
+console.log(window.innerHeight)
 
-const aniqlash = window.prompt("Raqam tanlang", 1)
-const tillar = [["Python", 1995], ["Java", 1998]]
-
-
-if (aniqlash == 1) {
-    console.log("Dasturlash tili " + tillar[0][0]
-    + " Ishlab chiqilgan yili " + tillar[0][1])
-} else if (aniqlash == 2) {
-    console.log("Dasturlash tili " + tillar[1][0]
-    + " Ishlab chiqilgan yili " + tillar[1][1])
-} else {
-    console.log("Bunday ma'lumot topilmadi")
-}
+window.open('https://susysacademy.uz', 'meningOynam', 'width=500,height=300')
