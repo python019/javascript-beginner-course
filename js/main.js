b2744ca9dae27c7d513e326605726a9aa1edd072
python019 <tommyjumayev@gmail.com>
2022-05-16 19:19:31 +0500

    20-dars

--- js/main.js
+++ js/main.js
@@ -1,10 +1,2 @@
-// JavaScript Location
+// JavaScript History
 
-
-console.log(window.location.href)
-
-// window.location.href 
-// window.location.hostname 
-// window.location.pathname 
-// window.location.protocol
-// window.location.assign()
