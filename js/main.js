3a48eb80706bfefd507f389fea9be45477875549
python019 <tommyjumayev@gmail.com>
2022-05-17 17:19:35 +0500

    23-dars

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 // JavaScript Foor loop
 
-for (i = 0; i > 20; i++) {
+mashinalar = ["BMW", "Mers", "Lambo", "Ford"];
+
+for (i = 0; i < 20; i++) {
+    if (i === 4) {
+        break
+    }
     console.log("malumot ishlamoqda")
+}
+
+for (let x in mashinalar) {
+    console.log(mashinalar[x])
 }
