96190a973fa2107c9b13e43806251e3c7ac307f4
python019 <tommyjumayev@gmail.com>
2022-05-19 01:27:48 +0500

    25-dars

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-// JavaScript Date
+// JavaScript Date Methods
 
-// ISO Sana 	"2015-03-25"
-const vaqt = new Date('2015-08-25')
+const days = ['0', "Dushanba","Seshanba","Chorshanba","Payshanba","Juma","Shanba", "Yakshanba"]
 
-// Qisqa Sana 	""
-const vaqt2 = new Date('03/25/2015')
+const vaqt = new Date()
 
-// Uzun Sana 	"Mar 25 2015" or "25 Mar 2015"
-const vaqt3 = new Date('Mar 25 2015')
 
-console.log(vaqt)
-console.log(vaqt2)
-console.log(vaqt3)
+console.log(days[7])
+
+// getFullYear() 	
+// getMonth() 	
+// getDate() 	
+// getHours() 
+// getMinutes() 	
+// getSeconds() 	
+// getMilliseconds() 	
+// getTime() 	
+// getDay()
