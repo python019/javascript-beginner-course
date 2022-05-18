72a5a5e13d5662544a23ddfe192ee1f4b01c6c1a
python019 <tommyjumayev@gmail.com>
2022-05-19 00:59:32 +0500

    24-dars

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-// JavaScript Foor loop
+// JavaScript Date
 
-mashinalar = ["BMW", "Mers", "Lambo", "Ford"];
+// ISO Sana 	"2015-03-25"
+const vaqt = new Date('2015-08-25')
 
-for (i = 0; i < 20; i++) {
-    if (i === 4) {
-        break
-    }
-    console.log("malumot ishlamoqda")
-}
+// Qisqa Sana 	""
+const vaqt2 = new Date('03/25/2015')
 
-for (let x in mashinalar) {
-    console.log(mashinalar[x])
-}
+// Uzun Sana 	"Mar 25 2015" or "25 Mar 2015"
+const vaqt3 = new Date('Mar 25 2015')
+
+console.log(vaqt)
+console.log(vaqt2)
+console.log(vaqt3)
